f0b6bc68f1d75b6285c203c2d82faf44bb73a45a
KhaledEssam <khaledessamabdulhadi@gmail.com>
2025-06-17 14:21:42 +0300

    built all details movies Section

--- js/script.js
+++ js/script.js
@@ -1,77 +1,140 @@
-// Start Swiper Js 
+// Start Swiper Js
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 5,
     },
-    breakpoints: {
-      "@0.00": {
-        slidesPerView: 1,
-        spaceBetween: 5,
-      },
-      "@0.75": {
-        slidesPerView: 2,
-        spaceBetween: 5,
-      },
-      "@1.00": {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-      "@1.50": {
-        slidesPerView: 4,
-        spaceBetween: 10,
-      },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 5,
     },
-  });
-// ENd Swiper Js 
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
+// ENd Swiper Js
 
 // Start Owl Carousel
-$(document).ready(function(){
-  var one = $("#subscription")
-  var two = $("#originals")
-    one.owlCarousel({
-        rtl:true,
-        loop:true,
-        margin:30,
-        nav:true,
-        responsive:{
-            0:{
-                items:2
-            },
-            600:{
-                items:4
-            },
-            1000:{
-                items:5
-            },
-            1200:{
-                items:6
-            }
-        }
-    });
-    two.owlCarousel({
-      ltr:true,
-      loop:true,
-      margin:30,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          },
-          1200:{
-              items:3
-          }
-      }
+$(document).ready(function () {
+  var one = $("#subscription");
+  var two = $("#originals");
+  var three = $("#popular");
+  var four = $("#newest-movies");
+  var five = $("#detailes-movies");
+  one.owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+      1200: {
+        items: 6,
+      },
+    },
   });
-  }); 
-// End Owl Carousel 
+  two.owlCarousel({
+    ltr: true,
+    loop: true,
+    margin: 30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+  three.owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+      1200: {
+        items: 6,
+      },
+    },
+  });
+  four.owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+      1200: {
+        items: 6,
+      },
+    },
+  });
+  five.owlCarousel({
+    ltr: true,
+    loop: true,
+    margin: 30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
+});
+// End Owl Carousel
 // Start Owl Carousel
-// End Owl Carousel 
+// End Owl Carousel
